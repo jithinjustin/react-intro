824bd52fc5ec471aad777193de3039639dbe68ff
jithin <jithinjustin@gmail.com>
2020-06-28 18:30:03 -0400

    convert to jsx, add react config to eslint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
-const Apps = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Birdie",
-      animal: "Bird",
-      breed: "Parrot",
-    }),
-    React.createElement(Pet, {
-      name: "Meow",
-      animal: "Cat",
-      breed: "exotic",
-    }),
-  ]);
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
 };
 
-render(React.createElement(Apps), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
