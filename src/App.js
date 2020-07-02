ac44b944750f456ed1798254f2c45cf1472ea970
jithin <jithinjustin@gmail.com>
2020-07-02 18:32:56 -0400

    reach router initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
-
+import { Router } from "@reach/router";
+import Details from "./Details";
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1>Adopt Me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
---
+++ src/Details.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Details = () => {
+  return <h1> hi lol</h1>;
+};
+export default Details;
