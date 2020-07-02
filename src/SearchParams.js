37e39991073465286b31ba47f802e1ae6c5cdfad
Jithin Justin <jithinjustin@users.noreply.github.com>
2020-07-02 13:27:45 -0400

    hooks, reusable dropdown component (#1)
    add hooks, reusable dropdown component

---
+++ src/SearchParams.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breeds, setBreeds] = useState([]);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </label>
+
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
