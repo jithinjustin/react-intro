751cdc847b8be4014fce57f2fd2eedb8165a1ddd
jithin <jithinjustin@gmail.com>
2020-07-02 18:42:13 -0400

    add Link , debugging tips

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 import Details from "./Details";
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1>Adopt Me!</h1>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+
         <Router>
           <SearchParams path="/" />
           <Details path="/details/:id" />
