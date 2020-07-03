ccbc2fdf8d8ba99ea12f945e6baf15afe60311ac
jithin <jithinjustin@gmail.com>
2020-07-03 16:34:08 -0400

    add errorboundary for error handling

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,12 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
   state = { loading: true };
   componentDidMount() {
+    //throw new Error("lol");
     pet
       .animal(this.props.id)
       .then(({ animal }) => {
@@ -41,4 +43,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
