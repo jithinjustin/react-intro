122ce5cd847a0dace137ad07409a7fa9065305e7
jithin <jithinjustin@gmail.com>
2020-07-03 15:52:24 -0400

    add carousel class component

--- src/Details.js
+++ src/Details.js
@@ -1,35 +1,35 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-    };
-  }
-
+  state = { loading: true };
   componentDidMount() {
-    pet.animal(this.props.id).then(({ animal }) => {
-      this.setState({
-        name: animal.name,
-        animal: animal.type,
-        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
-        description: animal.description,
-        media: animal.photos,
-        breed: animal.breeds.primary,
-        loading: false,
-      });
-    }, console.error);
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        });
+      })
+      .catch((err) => this.setState({ error: err }));
   }
-
   render() {
     if (this.state.loading) {
-      return <h1>loading...</h1>;
+      return <h1>loading … </h1>;
     }
-    const { animal, breed, location, description, name } = this.state;
+
+    const { animal, breed, location, description, media, name } = this.state;
+
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${location}`}</h2>
@@ -40,4 +40,5 @@ class Details extends React.Component {
     );
   }
 }
+
 export default Details;
